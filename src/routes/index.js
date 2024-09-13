29f0b2ffac3cc546ae7b15c53b1ba9c17e07efee
Leonardo <148665981+LeoCoelhoP@users.noreply.github.com>
2024-09-13 12:26:06 -0300

    Modify: Cron-job Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ router.use('/auth', authRoute);
 router.use('/exams', examRoute);
 router.use('/examTaken', examTakenRoute);
 router.use('/institutions', InstitutionRoute);
-router.use('cron-job', (req, res) => {
+router.use('/cron-job', (req, res) => {
   res.status(200).json({ message: 'successfully on!' });
 });
 
